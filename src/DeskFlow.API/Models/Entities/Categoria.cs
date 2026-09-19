764ce211ace1389635a43a6795ceb61503d89f61
Vlade <vladecassia@gmail.com>
2026-09-19 08:38:47 -0300

    feat: adiciona validações à entidade categoria

--- src/DeskFlow.API/Models/Entities/Categoria.cs
+++ src/DeskFlow.API/Models/Entities/Categoria.cs
@@ -1,6 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DeskFlow.API.Models.Entities;
 public class Categoria
 {
   public int Id { get; set; }
+  
+  [Required]
+  [MaxLength(100)]
   public string Nome { get; set; } = string.Empty;  
 }
