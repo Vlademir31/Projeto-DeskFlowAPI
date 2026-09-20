977ebe3db56bb7a8fbc0b8254be250a57c95dc7c
Vlade <vladecassia@gmail.com>
2026-09-20 14:35:55 -0300

    feat: registra depedências de categoria no DI

--- src/DeskFlow.API/Program.cs
+++ src/DeskFlow.API/Program.cs
@@ -1,4 +1,6 @@
 using DeskFlow.API.Data;
+using DeskFlow.API.Repository;
+using DeskFlow.API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer( builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<CategoriaRepository>();
+
+builder.Services.AddScoped<CategoriaService>();
 
 var app = builder.Build();
 
